52e115944ffd5200be30f5a8134b8ff8c4456e1f
chengyun <454166448@qq.com>
2018-08-02 11:17:00 -0400

    Print ab

---
+++ src/interview/question/printab/OrderThreadTest.java
@@ -0,0 +1,44 @@
+package interview.question.printab;
+
+/**
+ * Created by xuchengyun on 8/2/18.
+ */
+
+public class OrderThreadTest {
+
+    public static void main(String[] args) {
+
+        Object lock = new Object();
+        Thread t1 = new Thread(new Output(lock, "A"));
+        Thread t2 = new Thread(new Output(lock, "B"));
+        t1.start();
+        t2.start();
+
+
+    }
+}
+
+class Output implements Runnable {
+    private Object lock;
+    private String value;
+
+    public Output(Object lock, String value) {
+        this.lock = lock;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        synchronized (lock) {
+            while (true) {
+                try {
+                    System.out.println(value);
+                    lock.notifyAll();
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
